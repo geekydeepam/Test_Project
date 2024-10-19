8a8f43764adbc34ca9eb7c844aaeb1bace0478e1
deepam <78895124+geekydeepam@users.noreply.github.com>
2024-10-19 13:14:07 +0530

    Initial commit for ASP.NET MVC CRUD project

---
+++ Test_Project/Scripts/categoryShowHide.js
@@ -0,0 +1,22 @@
+﻿
+
+
+for (let i = 1; i <= 5; i++)
+{
+    document.getElementById("cat_"+i).addEventListener("mouseover", ShowCRUD)
+    document.getElementById("cat_"+i).addEventListener("mouseout", HideCRUD)
+
+
+
+    document.getElementById("crud_"+i).style.visibility="hidden"
+
+
+    function ShowCRUD() {
+        document.getElementById("crud_" + i).style.visibility = "visible"
+    }
+
+    function HideCRUD() {
+        document.getElementById("crud_" + i).style.visibility = "hidden"
+    }
+}
+
